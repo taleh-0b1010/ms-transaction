2464475a297b86a0ab00180af2e97fba37ae21f9
Taleh Gurbanzade <qtaleh41@gmail.com>
2023-08-03 16:53:10 +0300

    first commit

---
+++ src/test/java/com/pocopay/transaction/MsTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocopay.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
